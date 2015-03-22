9175333022e4549d15c7018f3449f300d3dd1d51
Sandeep <sandeeph47@gmail.com>
2015-03-22 17:52:30 +0530

    Added better explanation from java-forums.org

--- 2 Learning the Java Language/2.2 Language Basics/2.2.2 Operators/PrePostDemo.java
+++ 2 Learning the Java Language/2.2 Language Basics/2.2.2 Operators/PrePostDemo.java
@@ -3,21 +3,21 @@ class PrePostDemo {
 	public static void main(String[] args) {
 		
 		int i = 3;
-		i++;
+		i++;						// add 1 to i
 		// prints 4
-		System.out.println(i);
+		System.out.println(i);		// display this number
 		
-		++i;
+		++i;						// add 1 to i again
 		// prints 5
-		System.out.println(i);
+		System.out.println(i);		// display this number
 		
 		// prints 6
-		System.out.println(++i);
+		System.out.println(++i);	// first add 1 to i, then display it
 		
 		// prints 6
-		System.out.println(i++);
+		System.out.println(i++);	// first display i, then after its been displayed, add 1 to i
 		
 		// prints 7
-		System.out.println(i);
+		System.out.println(i);		// Number 7 
 	}
 }
